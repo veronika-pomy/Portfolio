5ed7287b7b7c02e07ae3bc20dc854ec44bc38609
Veronika Pomyateeva <pomyateevav@gmail.com>
2023-06-06 20:32:15 -0400

    configured screen size breakpoints, google fonts, and colors in tailwind config file

--- README.md
+++ README.md
@@ -6,12 +6,12 @@
 
 ### Attributions for icons used for projects
 
-https://www.flaticon.com/authors/eideticmemo
-https://www.flaticon.com/authors/freepik
+- https://www.flaticon.com/authors/eideticmemo
+- https://www.flaticon.com/authors/freepik
 
 ### Google Fonts
 
-https://fonts.google.com/specimen/Crimson+Text
-https://fonts.google.com/specimen/Mukta+Mahee
+- https://fonts.google.com/specimen/Crimson+Text
+- https://fonts.google.com/specimen/Mukta+Mahee
 
 ### React Icons
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#ac5159",
+        dark: "#2d3033",
+        light: "#f0f0f0",
+        blue: "#7d9fb9"
+      },
+      fontFamily: {
+        crimson: ["Crimson Text","serif"],
+        muktaam: ["Mukta Mahee", "sans-serif"]
+      },
+
+    },
+    screens: {
+      "xs": "480px",
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px"
+    }
   },
   plugins: [],
 }
